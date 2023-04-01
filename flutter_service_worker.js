e30a6b75fa30aa166bbc7328825a506186e7f29d
Deltarus <me33523@naver.com>
2023-04-01 18:23:55 +0900

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "a3d922a0b0bb96200631d1d9f758ce7d",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "215dbf833efe0d398190f2365d901ec6",
+"assets/fonts/MaterialIcons-Regular.otf": "e2aeabcc9d558151a831c7c0159a6ab7",
 "assets/NOTICES": "380dcb1c2d0c83ec3856c04452901c64",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/shaders/ink_sparkle.frag": "57f2f020e63be0dd85efafc7b7b25d80",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "a3d922a0b0bb96200631d1d9f758ce7d
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3e603488be55647203f191f10d183fda",
-"/": "3e603488be55647203f191f10d183fda",
-"main.dart.js": "4fcfcc73bf3f8ca1c6eb54fe44270859",
+"index.html": "6b8f9354e9b123789adb22302bb57d93",
+"/": "6b8f9354e9b123789adb22302bb57d93",
+"main.dart.js": "f1e6592549fae8361bac12605d5cd201",
 "manifest.json": "d0b040d4464a384818aac6ffae165f22",
 "version.json": "3221945076d11816c4d91242c47177ff"};
 // The application shell files that are downloaded before a service worker can
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:psalm_reminder/questions.dart';
-import 'package:psalm_reminder/questions_widgets.dart';
+import 'bible.dart';
+import 'bible_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,19 +19,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const Screen1(),
     );
   }
 }
 
-/*
- * 매인페이지
- * 시편 읽기 페이지
- * 장절 맞추기 퀴즈(무한생성)
-*/
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -39,13 +33,25 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: const Text("모두 같이 하는 성경공부")),
       body: Center(
         child: Column(children: [
-          ElevatedButton(onPressed: () => _buttonReading(context), child: const Text("성경 읽기")),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const MyHomePage()));
-              },
-              child: const Text("성경 퀴즈"))
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () => _buttonReading(context),
+              child: const Text("성경 읽기"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () => _buttonQuiz(context),
+                child: const Text("성경 퀴즈")),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () => _buttonWrite(context),
+                child: const Text("성경 쓰기")),
+          )
         ]),
       ),
     );
@@ -54,121 +60,100 @@ class HomePage extends StatelessWidget {
   void _buttonReading(BuildContext context) {
     Navigator.pop(context);
     Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => const ReadingPage(
-                  book: 'Psalms',
-                )));
+        context, MaterialPageRoute(builder: (context) => const ScreenChoose()));
+  }
+
+  void _buttonQuiz(BuildContext context) {
+    Navigator.pop(context);
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const ScreenQuizChoose()));
+  }
+
+  void _buttonWrite(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const ScreenWrite()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class ScreenReading extends StatefulWidget {
+  const ScreenReading({super.key, required this.type});
+  final BookType type;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ScreenReading> createState() => _ScreenReadingState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<int> verse = [2, 4];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("성경 암기용"),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
-          },
-        ),
-      ),
-      body: Center(
-          child: BibleQuestionWidget(
-        book: "Psalms",
-        current: verse,
-        tap: () {
-          Random r = Random();
-          late int newchap, newverse;
-          newchap = r.nextInt(Bible.bible["Psalms"]!.length - 1) + 1;
-          newverse = r.nextInt(Bible.bible["Psalms"]![newchap].length - 1) + 1;
-          setState(() {
-            verse = [newchap, newverse];
-          });
-        },
-      )),
-    );
-  }
-}
-
-class ReadingPage extends StatefulWidget {
-  const ReadingPage({super.key, required this.book});
-  final String book;
-
-  @override
-  State<ReadingPage> createState() => _ReadingPageState();
-}
-
-class _ReadingPageState extends State<ReadingPage> {
+class _ScreenReadingState extends State<ScreenReading> {
+  int page = 0;
   bool isMenu = true;
 
-  int page = 0;
-
-  /*
-   * 화면 구성은 리스트뷰로 절 단위로 띄워놓는 것. 
-   * 절당 앞쪽에 '1.' 이런 표시 있어서 몇 절인지 볼 수 있음
-   * 우측 상단에는 메뉴로 돌아가기, 다음 절, 이전 절로 갈 수 있는 아이콘 버튼을 띄워야 함
-  */
   @override
   Widget build(BuildContext context) {
-    List<List<String>> text = Bible.bible[widget.book]!;
     if (isMenu) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.book),
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
-            },
-          ),
-        ),
-        body: ListView.builder(
-            itemCount: text.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: const Icon(Icons.bookmark_border),
-                title: Text(text[index][1]),
-                subtitle: Text("$index장"),
-                onTap: () {
-                  setState(() {
-                    page = index;
-                    isMenu = false;
-                  });
-                },
-              );
-            }),
-      );
+      return _menuPage(context);
     }
+    return _readingPage(context);
+  }
+
+  void _gotoHome(BuildContext context) {
+    Navigator.pop(context);
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const ScreenChoose()));
+  }
+
+  Widget _menuPage(BuildContext context) {
+    final List<List<String>> text = Bible.bible[widget.type]!;
     return Scaffold(
       appBar: AppBar(
-        title: Text("${widget.book} $page"),
+        title: Text(widget.type.name),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () => _gotoHome(context),
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: Bible.bible[widget.type]!.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: const Icon(Icons.bookmark_border),
+              title: Text(text[index][1]),
+              subtitle: Text("${index + 1}"),
+              onTap: () {
+                setState(() {
+                  page = index;
+                  isMenu = false;
+                });
+              },
+            );
+          }),
+    );
+  }
+
+  Widget _readingPage(BuildContext context) {
+    final List<List<String>> text = Bible.bible[widget.type]!;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${widget.type.name} ${page + 1}"),
         leading: IconButton(
           icon: const Icon(
             Icons.arrow_back,
           ),
           onPressed: () {
-            Navigator.pop(context);
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
+            setState(() {
+              page = 0;
+              isMenu = true;
+            });
           },
         ),
         actions: [
+          IconButton(
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => _sameChapterQuizPage(context))),
+              icon: const Icon(Icons.document_scanner)),
           IconButton(
               onPressed: () {
                 if (page == 1) return;
@@ -202,5 +187,309 @@ class _ReadingPageState extends State<ReadingPage> {
             return ListTile(title: Text("$index. ${text[page][index]}"));
           }),
     );
+  }
+
+  StatefulBuilder _sameChapterQuizPage(BuildContext context) {
+    late Bible bib;
+    return StatefulBuilder(
+      builder: (BuildContext context, void Function(void Function()) setState) {
+        Random r = Random();
+        var v = r.nextInt(Bible.bible[widget.type]![page].length);
+        bib = Bible(widget.type, page, v);
+        return _sameChapterQuizPageWidget(context, bib, () {
+          var v = r.nextInt(Bible.bible[widget.type]![page].length);
+          setState(() {
+            bib = Bible(BookType.psalms, page, v);
+          });
+        });
+      },
+    );
+  }
+
+  Widget _sameChapterQuizPageWidget(
+      BuildContext context, Bible bible, AnswerTapping onTap) {
+    //BookType type = bible.book;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("성경 암기용"),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Center(
+          child: RandomBibleQuestionSelective(
+              sameChatper: true,
+              bible: bible,
+              onTap:
+                  onTap /*{
+          Random r = Random();
+          late int newverse;
+          newverse =
+              r.nextInt(Bible.bible[type]![bible.chapter].length - 1) + 1;
+          setState(() {
+            bible = Bible(type, bible.chapter, newverse);
+          });
+        },*/
+              )),
+    );
+  }
+}
+
+class ScreenChoose extends StatelessWidget {
+  const ScreenChoose({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Iterable iter = Bible.bible.keys;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("성경 읽기"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Screen1()));
+          },
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: iter.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text((iter.elementAt(index) as BookType).name),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ScreenReading(
+                            type: (iter.elementAt(index) as BookType))));
+              },
+            );
+          }),
+    );
+  }
+}
+
+class ScreenQuizChoose extends StatelessWidget {
+  const ScreenQuizChoose({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Iterable iter = Bible.bible.keys;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("성경 시험"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Screen1()));
+          },
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: iter.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text((iter.elementAt(index) as BookType).name),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ScreenQuiz(
+                              type: (iter.elementAt(index) as BookType),
+                            )));
+              },
+            );
+          }),
+    );
+  }
+}
+
+class SameChapterReading extends StatefulWidget {
+  final BookType type;
+
+  const SameChapterReading({
+    super.key,
+    required this.type,
+  });
+
+  @override
+  State<SameChapterReading> createState() => _SameChapterReadingState();
+}
+
+class _SameChapterReadingState extends State<SameChapterReading> {
+  int page = 0;
+  @override
+  Widget build(BuildContext context) {
+    //Navigator.push(context, MaterialPage(child: StatefulWidgetBuilder))
+    Bible bible = Bible(BookType.psalms, page, 1);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("성경 암기용"),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Center(
+          child: RandomBibleQuestionSelective(
+        sameChatper: true,
+        bible: bible,
+        onTap: () {
+          Random r = Random();
+          late int newverse;
+          newverse =
+              r.nextInt(Bible.bible[widget.type]![bible.chapter].length - 1) +
+                  1;
+          setState(() {
+            bible = Bible(widget.type, bible.chapter, newverse);
+          });
+        },
+      )),
+    );
+  }
+}
+
+class ScreenQuiz extends StatefulWidget {
+  const ScreenQuiz({super.key, required this.type});
+  final BookType type;
+
+  @override
+  State<ScreenQuiz> createState() => _ScreenQuizState();
+}
+
+class _ScreenQuizState extends State<ScreenQuiz> {
+  late Bible bible = Bible(widget.type, 0, 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("성경 암기용"),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () => _exit(context),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () => _review(context),
+              icon: const Icon(Icons.book_outlined)),
+        ],
+      ),
+      body: Center(
+          child: RandomBibleQuestionSelective(
+        bible: bible,
+        onTap: () {
+          Random r = Random();
+          late int newchap, newverse;
+          newchap = r.nextInt(Bible.bible[widget.type]!.length);
+          newverse = r.nextInt(Bible.bible[widget.type]![newchap].length);
+          setState(() {
+            bible = Bible(widget.type, newchap, newverse);
+          });
+        },
+      )),
+    );
+  }
+
+  void _exit(BuildContext context) {
+    Navigator.pop(context);
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const Screen1()));
+  }
+
+  void _review(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => BibleTextViewerWidget(
+                  bible: bible,
+                )));
+  }
+}
+
+class ScreenWrite extends StatefulWidget {
+  const ScreenWrite({super.key});
+
+  @override
+  State<ScreenWrite> createState() => _ScreenWriteState();
+}
+
+class _ScreenWriteState extends State<ScreenWrite> {
+  BookType type = BookType.psalms;
+  late Bible bible = Bible(type, 2, 4);
+  bool isHide = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("복 있는 사람은 오직 여호와의 율법만 주야로 묵상하는도다"),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () => _hide(), icon: const Icon(Icons.hide_source)),
+            IconButton(
+                onPressed: () => _review(context),
+                icon: const Icon(Icons.book_outlined))
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: WriteRandomVerseWidget(
+            bible: bible,
+            onPressed: () => _update(),
+            isHide: isHide,
+          ),
+        ));
+  }
+
+  void _update() {
+    setState(() {
+      bible = _new();
+    });
+  }
+
+  void _hide() {
+    setState(() {
+      if (isHide) {
+        isHide = false;
+      } else {
+        isHide = true;
+      }
+    });
+  }
+
+  void _review(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => BibleTextViewerWidget(
+                  bible: bible,
+                  compact: true,
+                )));
+  }
+
+  Bible _new() {
+    Random r = Random();
+    var nChap = r.nextInt(Bible.bible[type]!.length - 1) + 1;
+    var nVers = r.nextInt(Bible.bible[type]![nChap].length - 1) + 1;
+    Bible newBible = Bible(type, nChap, nVers);
+
+    return newBible;
   }
 }
