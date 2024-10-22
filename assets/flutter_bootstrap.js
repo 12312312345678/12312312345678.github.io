e7c5bde0d4d6008e788190d0177f03d09d7efd8c
Deltarus <me33523@naver.com>
2024-10-23 07:55:48 +0900

    Add files via upload

--- assets/flutter_bootstrap.js
+++ assets/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4204322948"
+    serviceWorkerVersion: "2909683685"
   }
 });
